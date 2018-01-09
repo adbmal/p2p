396fb472931c278e0f55a20a3a0f3705abf3af4c
Yiwei <1wei@live.com>
2018-01-09 17:31:36 +0800

    cmake init

---
+++ demo.cxx
@@ -0,0 +1,8 @@
+// A simple program that computes the square root of a number
+#include <stdio.h>
+#include <stdlib.h>
+int main (int argc, char *argv[])
+{
+  fprintf(stdout,"Hello\n");
+  return 0;
+}
